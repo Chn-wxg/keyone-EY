c89e86e15bb34ac5bfb872ca78f9dc8782efc8f3
Chn-wxg <68806889+Chn-wxg@users.noreply.github.com>
2020-10-25 15:19:14 +0800

    Add files via upload

--- keyone-EY.sh
+++ keyone-EY.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/Chn-wxg/keyone-EY/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/Chn-wxg/keyone-EY/main/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/Chn-wxg/keyone-EY/master/user.conf
+wget https://raw.githubusercontent.com/Chn-wxg/keyone-EY/main/user.conf
 nginx -s reload
 
